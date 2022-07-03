f81add7363dbb15185cef203e8e425ac0edfac7f
Pasha Shalunov <p.shalunov@iteco-inno.ru>
2022-07-03 11:49:40 +0400

    3.6 pytest

--- test_items.py
+++ test_items.py
@@ -8,4 +8,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button(browser):
     browser.get(link)
     time.sleep(5)
-    browser.find_element(By.XPATH, "//button[text() = 'Añadir al carrito']")
+    button_add = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')[0].text
+    assert button_add == 'Añadir al carrito', "Текст кнопки не совпадает"
